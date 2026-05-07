d38f59258a9669a2951c36ee52ff18647308213c
maxime.kyriakides <maxime.kyriakides@viseo.com>
2026-05-07 14:38:44 +0200

    wip

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,14 +12,22 @@ import { SearchBar } from './components/search-bar/search-bar';
 })
 export class App {
   coin: CollectionItem;
+  ronflex: CollectionItem;
 
   constructor() {
     this.coin = new CollectionItem(); 
     this.coin.name = "Coin";
     this.coin.description = "A shiny gold coin.";
-    this.coin.price = 1.99;
+    this.coin.price = 3.99;
     this.coin.rarity = "Common";
     this.coin.image = "img/pokecoin.png";
+
+    this.ronflex = new CollectionItem();
+    this.ronflex.name = "Ronflex";
+    this.ronflex.description = "Ronflex is a Normal-type Pokémon from Generation I. It’s very large and often sleeps in the way, blocking paths. Despite being lazy, it’s powerful, with high HP and strong physical attacks.";
+    this.ronflex.price = 19.99;
+    this.ronflex.rarity = "Rare";
+    this.ronflex.image = "img/ronflex.png";
   }
 
   count : number = 0;
